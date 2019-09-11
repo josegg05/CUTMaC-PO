1a6cb5d8214dc7ef62d54b98b15de8963551c69e
josegg05 <42848827+josegg05@users.noreply.github.com>
2019-09-11 18:51:26 -0300

    First Test

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print("hello")
+from snakes.nets import *
+n = PetriNet('First net')
+n.add_place(Place('p', [0]))
+n.add_transition(Transition('t', Expression('x<5')))
+n.add_input('p', 't', Variable('x'))
+n.add_output('p', 't', Expression('x+1'))
+
+modes = n.transition('t').modes()
+print(modes)
+
+n.transition('t').fire(Substitution(x=0))
+
+state = n.get_marking()
+print(state)
