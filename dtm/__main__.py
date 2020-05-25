e79a8c476b8cc8c25504b6388ed934e6061fefce
josegg05 <42848827+josegg05@users.noreply.github.com>
2020-05-25 00:43:24 -0400

    cutmapnet v4.9.4: - Using congestion model 2 - zmq pipes clean at the en of the loop

--- dtm/__main__.py
+++ dtm/__main__.py
@@ -494,13 +494,12 @@ if __name__ == '__main__':
     # Reset Loop
     while True:
         msg_dic = []
+        run()
+        run_num += 1
+        time.sleep(10)
         # Empty the zmq pipe
         poll = dict(poller.poll(20))
         while sub_socket in poll and poll[sub_socket] == zmq.POLLIN:
             [top, contents] = sub_socket.recv_multipart()
             poll = dict(poller.poll(20))
-
-        run()
-        run_num += 1
-        time.sleep(30)
-
+        time.sleep(20)
