614f1710060cd8423a5983f068a75155b0fb3e5f
josegg05 <42848827+josegg05@users.noreply.github.com>
2020-05-26 19:51:44 -0400

    cutmapnet v4.9.8: - supervisor split_pi_model: congestion variable range from (0, 101) --> (0, 81)

--- supervisor/__main__.py
+++ supervisor/__main__.py
@@ -96,9 +96,9 @@ def set_five_quant_label(level):
 
 
 def split_pi_model_conf():
-    my_congestion_level = ctrl.Antecedent(np.arange(0, 101, 1), 'my_congestion_level')  # Antes 101
-    in_congestion_level = ctrl.Antecedent(np.arange(0, 101, 1), 'in_congestion_level')  # Antes 101
-    out_congestion_level = ctrl.Antecedent(np.arange(0, 101, 1), 'out_congestion_level')  # Antes 101
+    my_congestion_level = ctrl.Antecedent(np.arange(0, 81, 1), 'my_congestion_level')  # Antes 101
+    in_congestion_level = ctrl.Antecedent(np.arange(0, 81, 1), 'in_congestion_level')  # Antes 101
+    out_congestion_level = ctrl.Antecedent(np.arange(0, 81, 1), 'out_congestion_level')  # Antes 101
     split = ctrl.Consequent(np.arange(-2, 3, 1), 'split')
 
     # Membership Functions definition
